97f7931bb369bd0f303cbcfc12be60ffdee94428
Alec Flett <alecf@flett.org>
2013-09-04 18:01:27 -0700

    big cleanup - get rid of all the transaction-caching junk that was there before

--- graphdb/graphdb.js
+++ graphdb/graphdb.js
@@ -36,6 +36,7 @@
 function GraphDB(name) {
     var graphDB = this;
 
+    graphDB.readyCallbacks = [];
     graphDB.ready = false;
     var keyPaths = graphDB.getKeyPaths();
     var firstKeyPath = keyPaths[keyPaths.length-1];
@@ -43,14 +44,10 @@ function GraphDB(name) {
     graphDB.dbName = "graphDB-" + name;
     var req = indexedDB.open(graphDB.dbName, 4);
     req.onupgradeneeded = function(e) {
-        graphDB.db = e.target.result;
+        var db = e.target.result;
         console.log("Creating GraphDB: " + graphDB.dbName, e.target.result);
-        if ('graph' in e.target.result.objectStoreNames) {
-            console.log("Creating graph");
-            graphDB.db.deleteObjectStore('graph');
-        }
-        if (!('graph' in graphDB.db.objectStoreNames)) {
-            var objectStore = graphDB.db.createObjectStore('graph', {keyPath: firstKeyPath});
+        if (!('links' in db.objectStoreNames)) {
+            var objectStore = db.createObjectStore('links', {keyPath: firstKeyPath});
             for (var i = 0; i < keyPaths.length; i++) {
                 var keyPath = keyPaths[i];
                 objectStore.createIndex(keyPath.join(','), keyPath);
@@ -62,58 +59,23 @@ function GraphDB(name) {
     req.onsuccess = function(e) {
         graphDB.db = e.target.result;
         graphDB.ready = true;
-        if (graphDB.readyCallback)
-            graphDB.readyCallback();
+        graphDB.checkReady();
     };
 }
 
-GraphDB.prototype.whenReady = function(f) {
-    if (this.ready)
-        f();
-    else
-        this.readyCallback = f;
-};
-
-// creates an entirely new transaction with the given mode
-GraphDB.prototype.transaction = function(mode) {
-    return this.graph.transaction(mode);
-};
-
-// Get the current transaction if it is still active. Note that this
-// is a little funky, it assumes that lastRequest_ is always kept up
-// to date. It would be nice if the current transaction could be
-// queried for active-ness or not.
-GraphDB.prototype.transaction_ = function() {
-    // FIXME: right now everything is readwrite
-    if (this.current_transaction_ &&
-        this.lastRequest_ &&
-        this.lastRequest_.readyState == "pending") {
-        // console.log("Reusing transaction from:");
-        // console.trace();
-
-    } else {
-        // console.log("Creating transaction from:");
-        // console.trace();
-        var graphDB = this;
-        this.current_transaction_ = this.db.transaction("graph", "readwrite");
-        var clear_all_timer = function() {
-            // console.log("Clearing from a timer, ", graphDB.current_transaction_ ?
-            //             " still around " : " already cleared");
-                delete graphDB.lastRequest_;
-                delete graphDB.current_transaction_;
-            };
-        var clear_all_oncomplete = function() {
-            // console.log("Clearing from oncomplete, ", graphDB.current_transaction_ ?
-            //             " still around " : " already cleared");
-                delete graphDB.lastRequest_;
-                delete graphDB.current_transaction_;
-            };
-        this.current_transaction_.oncomplete = clear_all_oncomplete;
-        // would be nice not to need this!
-        setTimeout(clear_all_timer, 1);
-
+GraphDB.prototype.checkReady = function() {
+    if (!this.ready)
+        return;
+    var callback = this.readyCallbacks.pop();
+    while (callback) {
+        callback(this);
+        callback = this.readyCallbacks.pop();
     }
-    return this.current_transaction_;
+}
+
+GraphDB.prototype.whenReady = function(f) {
+    this.readyCallbacks.push(f);
+    this.checkReady();
 };
 
 GraphDB.INDEX_FIELDS = ["source", "property", "target"];
@@ -155,73 +117,46 @@ GraphDB.combinations = function(list, len, ignore, indent) {
     return result;
 };
 
-GraphDB.prototype.add = function(edge, transaction) {
+GraphDB.prototype.add = function(edge) {
     var graphDB = this;
-    var trans = transaction || this.transaction_();
-    try {
-        this.lastRequest_ = trans.objectStore("graph").put(edge);
-        return this.lastRequest_;
-    } catch(E) {
-        console.log("error adding ", edge, ": ", E);
-        delete this.lastRequest_;
-        delete this.current_tansaction_;
-
-        // try once again
-        trans = transaction || this.transaction_();
-        this.lastRequest_ = trans.objectStore("graph").put(edge);
-        return this.lastRequest_;
-        throw E;
-    }
+    var transaction = this.db.transaction('links', 'readwrite');
+    return transaction.objectStore('links').put(edge);
 };
 
-GraphDB.prototype.find = function(edgeTemplate, transaction) {
+GraphDB.prototype.find = function(edgeTemplate) {
     if (!edgeTemplate)
         edgeTemplate = {};
 
     var graphDB = this;
-
+    var transaction = graphDB.db.transaction('links');
     var query = graphDB.getIndexAndKey(edgeTemplate, transaction);
-    try {
-        this.lastRequest_ = query.index.openCursor(query.key);
-    } catch (e) {
-        delete this.lastRequest_;
-        this.lastRequest_ = query.index.openCursor(query.key);
-    }
-    return this.lastRequest_;
+    return query.index.openCursor(query.key);
 };
 
-GraphDB.prototype.find_intersect = function() {
-    var requests = [];
-    for (var i = 0; i < arguments.length; i++) {
-        requests.push(this.find(arguments[i]));
-    }
-
-};
-
-GraphDB.prototype.count = function(edgeTemplate, transaction) {
+GraphDB.prototype.count = function(edgeTemplate) {
     if (!edgeTemplate)
         edgeTemplate = {};
 
     var graphDB = this;
-
+    var transaction = graphDB.db.transaction('links');
     var query = graphDB.getIndexAndKey(edgeTemplate, transaction);
-    this.lastRequest_ = query.index.count(query.key);
-    return this.lastRequest_;
+    return query.index.count(query.key);
 };
 
-GraphDB.prototype.groupby = function(properties, template, transaction) {
+GraphDB.prototype.groupby = function(properties, template) {
     var fakeTemplate = {};
     for (var i = 0; i < properties.length; i++) {
         fakeTemplate[properties[i]] = true;
     };
 
     var graphDB = this;
+    var transaction = this.transaction('links');
     var query = graphDB.getIndexAndKey(fakeTemplate, transaction);
     var result = {};
     console.log("groupby opening cursor");
-    this.lastRequest_ = query.index.openKeyCursor();
-    this.lastRequest_.onsuccess = GraphDB.emit_unique(result);
-    this.lastRequest_.onerror = function(e) {
+    var request = query.index.openKeyCursor();
+    request.onsuccess = GraphDB.emit_unique(result);
+    request.onerror = function(e) {
         console.log("Error opening cursor");
         result.onerror(e);
     };
@@ -288,8 +223,7 @@ GraphDB.prototype.getIndexAndKey = function(edgeTemplate, transaction) {
         if (fieldName in edgeTemplate && edgeTemplate[fieldName] !== null)
             presentFields.push(fieldName);
     }
-    var trans = transaction || this.transaction_();
-    var objectStore = trans.objectStore("graph");
+    var objectStore = transaction.objectStore('links');
     if (presentFields.length) {
         var key = [];
         for (i = 0; i < presentFields.length; i++)
